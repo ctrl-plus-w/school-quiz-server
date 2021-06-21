afc3dcb9806645a8d701d818f00e3c644a19873c
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-06-21 19:28:50 +0200

    Added a route and a package.
    - Added morgan (logger).
    - Added the auth router and controller.
    - Created the login route.
    - Temporary removed the db sync and seeder.

---
+++ src/api/auth/auth.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from 'express';
+import { sign, SignOptions } from 'jsonwebtoken';
+
+import bcrypt from 'bcrypt';
+
+import { User } from '../../models/user';
+import { Role } from '../../models/role';
+
+import credentials from '../../constants/credentials';
+
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  try {
+    const user = await User.findOne({ where: { username }, include: Role });
+    if (!user) throw new Error();
+
+    const passwordValid = await bcrypt.compare(password, user.password);
+    if (!passwordValid) throw new Error();
+
+    const payload = {
+      username: user.username,
+      role: user.role?.slug,
+    };
+
+    const options: SignOptions = {
+      expiresIn: 1,
+    };
+
+    const token = sign(payload, credentials.JWT_TOKEN, options);
+
+    res.json({ token });
+  } catch (error) {
+    res.status(400).json({ message: "Le nom d'utilisateur ou le mot de passe est invalide." });
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 // Imports
 import express from 'express';
+import morgan from 'morgan';
+
 import { json } from 'body-parser';
 
 import database from './models';
 import registerAssociations from './database/registerAssociations';
 import seedDatabase from './database/seedDatabase';
+
+import auth from './api/auth/auth.routes';
 
 // Constants
 const PORT = process.env.PORT || 6000;
@@ -13,24 +17,17 @@ const PORT = process.env.PORT || 6000;
 const app: express.Application = express();
 
 // Middlewares
+app.use(morgan('dev'));
 app.use(json());
 
-app.get('/', (_: express.Request, res: express.Response) => {
-  res.json({
-    message: 'Welcome on the api.',
-  });
-});
-
-app.get('/users', async (_, res) => {
-  const users = await database.models.User.findAll();
-  res.json({ users });
-});
+// Routes
+app.use('/auth', auth);
 
 (async () => {
   await registerAssociations();
 
-  await database.sequelize.sync({ force: true });
-  await seedDatabase();
+  // await database.sequelize.sync({ force: true });
+  // await seedDatabase();
 
   app.listen(PORT, () => {
     console.log(`App started, listening on port : ${PORT}.`);
