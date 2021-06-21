34213de67831d7b2e8e6f9ef1c11883fdd69cdf7
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-06-21 20:13:49 +0200

    Added / modified constants.
    - Added a jwt_token variable to the credentials.
    - Added a role file with the roles and their permissions.

--- src/constants/credentials.ts
+++ src/constants/credentials.ts
@@ -1,4 +1,4 @@
 export default {
   DB_URL: process.env.DB_URL || 'mysql://root:!FreshRoot1@localhost/SchoolQuiz',
-  JWT_TOKEN: process.env.JWT_TOKEN || 'z5dùa65az4d65!;',
+  JWT_TOKEN: process.env.JWT_TOKEN || 'az4d1654',
 };
---
+++ src/constants/roles.ts
@@ -0,0 +1,11 @@
+export default {
+  ADMIN: {
+    PERMISSION: 1,
+  },
+  PROFESSOR: {
+    PERMISSION: 2,
+  },
+  STUDENT: {
+    PERMISSION: 3,
+  },
+};
