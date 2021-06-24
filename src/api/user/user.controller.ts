5059c14ced0f8e466eb822c173710f5daf04771e
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-06-24 18:51:33 +0200

    Modifed the user controller / routes.
    - Added the group routes. (get, add, remove)
    - Added the role routes. (get, set)
    - Modifed the error handling for the create user validation schema.

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -5,14 +5,16 @@ import bcrypt from 'bcrypt';
 
 import { User } from '../../models/user';
 
-import { DuplicationError, NotFoundError } from '../../classes/StatusError';
+import { DuplicationError, InvalidInputError, NotFoundError } from '../../classes/StatusError';
+import { Group } from '../../models/group';
+import { Role } from '../../models/role';
 
 const schema = Joi.object({
   username: Joi.string().min(5).max(25).required(),
   firstName: Joi.string().min(5).max(25).required(),
   lastName: Joi.string().min(5).max(25).required(),
   password: Joi.string().min(7).max(35).required(),
-  gender: Joi.boolean(),
+  gender: Joi.boolean().allow(null),
 });
 
 export const getUsers = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
@@ -28,6 +30,52 @@ export const getUser = async (req: Request, res: Response, next: NextFunction): 
   try {
     const user = await User.findByPk(req.params.userId);
     res.json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getGroups = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const userId = req.params.userId;
+    if (!userId) return next(new InvalidInputError());
+
+    const user = await User.findByPk(userId, { include: Group });
+    if (!user) return next(new NotFoundError('User'));
+
+    res.json(user.groups);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getGroup = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const userId = req.params.userId;
+    const groupId = req.params.groupId;
+    if (!userId || !groupId) return next(new InvalidInputError());
+
+    const user = await User.findByPk(userId, { include: Group });
+    if (!user) return next(new NotFoundError('User'));
+
+    const group = user.groups?.find((group) => group.id === +groupId);
+    if (!group) return next(new NotFoundError('Group'));
+
+    res.json(group);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getRole = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const userId = req.params.userId;
+    if (!userId) return next(new InvalidInputError());
+
+    const user = await User.findByPk(userId, { include: Role });
+    if (!user) return next(new NotFoundError('User'));
+
+    res.json(user.role);
   } catch (err) {
     next(err);
   }
@@ -49,7 +97,8 @@ export const createUser = async (req: Request, res: Response, next: NextFunction
       gender: boolean | null;
     } = req.body;
 
-    await schema.validateAsync(req.body);
+    const validation = await schema.validateAsync(req.body).catch(console.log);
+    if (validation instanceof Error) return next(new InvalidInputError());
 
     const user = await User.findOne({ where: { username: username } });
     if (user) return next(new DuplicationError('User'));
@@ -69,12 +118,78 @@ export const createUser = async (req: Request, res: Response, next: NextFunction
   }
 };
 
+export const addGroup = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const userId = req.params.userId;
+    const groupId = req.body.groupId;
+    if (!userId || !groupId) return next(new InvalidInputError());
+
+    const user = await User.findByPk(userId, { include: Group });
+    if (!user) return next(new NotFoundError('User'));
+
+    if (user.groups?.some((group) => group.id === groupId)) return next(new DuplicationError('Group'));
+
+    const group = await Group.findByPk(groupId);
+    if (!group) return next(new NotFoundError('Group'));
+
+    await user.addGroup(group);
+
+    res.json({ added: true });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const setRole = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const userId = req.params.userId;
+    const roleId = req.body.roleId;
+    if (!userId || !roleId) return next(new InvalidInputError());
+
+    const user = await User.findByPk(userId, { include: Role });
+    if (!user) return next(new NotFoundError('User'));
+
+    if (user.role?.id === roleId) return next(new DuplicationError('Role'));
+
+    const role = await Role.findByPk(roleId);
+    if (!role) return next(new NotFoundError('Role'));
+
+    await user.setRole(role);
+
+    res.json({ set: true });
+  } catch (err) {
+    next(err);
+  }
+};
+
 export const deleteUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const user = await User.findByPk(req.params.userId);
     if (!user) return next(new NotFoundError('User'));
 
     await user.destroy();
+
+    res.json({ deleted: true });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const removeGroup = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const userId = req.params.userId;
+    const groupId = req.body.groupId;
+    if (!userId || !groupId) return next(new InvalidInputError());
+
+    const user = await User.findByPk(userId, { include: Group });
+    if (!user) return next(new NotFoundError('User'));
+
+    if (!user.groups?.some((group) => group.id === groupId)) return next(new NotFoundError('Group'));
+
+    const group = await Group.findByPk(groupId);
+    if (!group) return next(new NotFoundError('Group'));
+
+    await user.removeGroup(group);
 
     res.json({ deleted: true });
   } catch (err) {
--- src/api/user/user.routes.ts
+++ src/api/user/user.routes.ts
@@ -1,6 +1,17 @@
 import { Router } from 'express';
 
-import { createUser, deleteUser, getUser, getUsers } from './user.controller';
+import {
+  createUser,
+  deleteUser,
+  getRole,
+  getUser,
+  getUsers,
+  getGroups,
+  getGroup,
+  setRole,
+  addGroup,
+  removeGroup,
+} from './user.controller';
 
 import checkPermission from '../../middlewares/authorization.middleware';
 
@@ -10,9 +21,15 @@ const router = Router();
 
 router.get('/', checkPermission(roles.ADMIN.PERMISSION), getUsers);
 router.get('/:userId', checkPermission(roles.ADMIN.PERMISSION), getUser);
+router.get('/:userId/role', checkPermission(roles.ADMIN.PERMISSION), getRole);
+router.get('/:userId/groups', checkPermission(roles.ADMIN.PERMISSION), getGroups);
+router.get('/:userId/groups/:groupId', checkPermission(roles.ADMIN.PERMISSION), getGroup);
 
 router.post('/', checkPermission(roles.ADMIN.PERMISSION), createUser);
+router.post('/:userId/role', checkPermission(roles.ADMIN.PERMISSION), setRole);
+router.post('/:userId/groups', checkPermission(roles.ADMIN.PERMISSION), addGroup);
 
 router.delete('/:userId', checkPermission(roles.ADMIN.PERMISSION), deleteUser);
+router.delete('/:userId/groups', checkPermission(roles.ADMIN.PERMISSION), removeGroup);
 
 export default router;
