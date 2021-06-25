540ccc2f36cb876357327be70b031841f267dc02
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-06-25 12:14:00 +0200

    Modified the role creation error handling.
    - Exported the creation attributes from the role model.
    - Set the schema validation method as not async.

--- src/models/role.ts
+++ src/models/role.ts
@@ -7,7 +7,7 @@ interface RoleAttributes {
   permission: number;
 }
 
-type RoleCreationAttributes = Optional<RoleAttributes, 'id'>;
+export type RoleCreationAttributes = Optional<RoleAttributes, 'id'>;
 
 export class Role extends Model<RoleAttributes, RoleCreationAttributes> implements RoleAttributes {
   public id!: number;
