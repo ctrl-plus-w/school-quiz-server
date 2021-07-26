035fac02aaf7e5d6bee638cbcd6b08b9b5db65fe
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-07-26 22:54:35 +0200

    Removed delete routes for typed answers

--- src/api/comparisonAnswer/comparisonAnswer.routes.ts
+++ src/api/comparisonAnswer/comparisonAnswer.routes.ts
@@ -1,14 +1,12 @@
 import { Router } from 'express';
 
-import { deleteComparisonAnswer, getComparisonAnswer, getComparisonAnswers } from './comparisonAnswer.controller';
+import { getComparisonAnswer, getComparisonAnswers } from './comparisonAnswer.controller';
 
-import { authorize, checkIsAdmin, checkIsProfessor } from '../../middlewares/authorization.middleware';
+import { authorize, checkIsProfessor } from '../../middlewares/authorization.middleware';
 
 const router = Router();
 
 router.get('/', authorize([checkIsProfessor]), getComparisonAnswers);
 router.get('/:comparisonAnswerId', authorize([checkIsProfessor]), getComparisonAnswer);
-
-router.delete('/:comparisonAnswerId', authorize([checkIsAdmin]), deleteComparisonAnswer);
 
 export default router;
--- src/api/exactAnswer/exactAnswer.routes.ts
+++ src/api/exactAnswer/exactAnswer.routes.ts
@@ -1,14 +1,12 @@
 import { Router } from 'express';
 
-import { deleteExactAnswer, getExactAnswer, getExactAnswers } from './exactAnswer.controller';
+import { getExactAnswer, getExactAnswers } from './exactAnswer.controller';
 
-import { authorize, checkIsAdmin, checkIsProfessor } from '../../middlewares/authorization.middleware';
+import { authorize, checkIsProfessor } from '../../middlewares/authorization.middleware';
 
 const router = Router();
 
 router.get('/', authorize([checkIsProfessor]), getExactAnswers);
 router.get('/:exactAnswerId', authorize([checkIsProfessor]), getExactAnswer);
-
-router.delete('/:exactAnswerId', authorize([checkIsAdmin]), deleteExactAnswer);
 
 export default router;
