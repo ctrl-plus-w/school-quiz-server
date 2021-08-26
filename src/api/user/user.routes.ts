15dda7646ca558a4d53915ddf7f54ad97bba2044
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-08-26 18:58:41 +0200

    Fixed the user routes permission

--- src/api/user/user.routes.ts
+++ src/api/user/user.routes.ts
@@ -20,20 +20,20 @@ import {
   getUserEvent,
 } from './user.controller';
 
-import { authorize, checkIsAdmin, checkIsProfessor } from '../../middlewares/authorization.middleware';
+import { authorize, checkIsAdmin, checkIsStudent } from '../../middlewares/authorization.middleware';
 
 const router = Router();
 
-router.get('/', authorize([checkIsProfessor]), getUsers);
-router.get('/:userId', authorize([checkIsProfessor]), getUser);
-router.get('/:userId/role', authorize([checkIsProfessor]), getUserRole);
-router.get('/:userId/state', authorize([checkIsProfessor]), getUserState);
-router.get('/:userId/groups', authorize([checkIsProfessor]), getUserGroups);
-router.get('/:userId/groups/:groupId', authorize([checkIsProfessor]), getUserGroup);
-router.get('/:userId/events', authorize([checkIsProfessor]), getUserEvents);
-router.get('/:userId/events/:eventId', authorize([checkIsProfessor]), getUserEvent);
-router.get('/:userId/quizzes', authorize([checkIsProfessor]), getUserQuizzes);
-router.get('/:userId/quizzes/:quizId', authorize([checkIsProfessor]), getUserQuiz);
+router.get('/', authorize([checkIsStudent]), getUsers);
+router.get('/:userId', authorize([checkIsStudent]), getUser);
+router.get('/:userId/role', authorize([checkIsStudent]), getUserRole);
+router.get('/:userId/state', authorize([checkIsStudent]), getUserState);
+router.get('/:userId/groups', authorize([checkIsStudent]), getUserGroups);
+router.get('/:userId/groups/:groupId', authorize([checkIsStudent]), getUserGroup);
+router.get('/:userId/events', authorize([checkIsStudent]), getUserEvents);
+router.get('/:userId/events/:eventId', authorize([checkIsStudent]), getUserEvent);
+router.get('/:userId/quizzes', authorize([checkIsStudent]), getUserQuizzes);
+router.get('/:userId/quizzes/:quizId', authorize([checkIsStudent]), getUserQuiz);
 
 router.post('/', authorize([checkIsAdmin]), createUser);
 router.post('/:userId/groups', authorize([checkIsAdmin]), addGroup);
