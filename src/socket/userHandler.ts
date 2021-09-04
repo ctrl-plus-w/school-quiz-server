16254c9a50486b9fc9d168131050f526420865e0
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-09-04 11:35:11 +0200

    Made the user:join socketio  event

--- src/socket/userHandler.ts
+++ src/socket/userHandler.ts
@@ -1,42 +1,52 @@
-import { Op } from 'sequelize';
+import { Includeable, Op } from 'sequelize';
 import { Event } from '../models/event';
 import { Group } from '../models/group';
 import { State } from '../models/state';
+import { User } from '../models/user';
 
 import { ISocketWithJWT } from '../types/auth.types';
 
 export default (socket: ISocketWithJWT): void => {
   const isDev = process.env.NODE_ENV === 'development';
 
-  socket.on('user:join', async (payload: { eventId?: number }) => {
-    if (!payload.eventId) return;
-
+  socket.on('user:join', async () => {
     const user = socket.user;
+    const isStudent = socket.jwt.role === 'eleve';
 
-    if (socket.jwt.role === 'eleve') {
-      await socket.join([`student-event-${payload.eventId}`, `event-${payload.eventId}`]);
-      isDev && console.log(`${user.username} joined room student-event-${payload.eventId} and event-${payload.eventId} as a student.`);
+    const groups = await (isStudent ? user.getGroups({ attributes: ['id'] }) : <Array<Group>>[]);
 
-      const groups = await user.getGroups();
-      if (!groups || groups.length === 0) return;
+    const eventIncludes: Includeable[] = isStudent
+      ? [{ model: Group, where: { id: groups.map(({ id }) => id) }, attributes: ['id'] }]
+      : [{ model: User, as: 'owner', where: { id: user.id } }];
 
-      const groupsId = groups.map(({ id }) => id);
+    const actualEvent = await Event.findOne({
+      where: { start: { [Op.lte]: new Date() }, end: { [Op.gte]: new Date() } },
+      include: [...eventIncludes],
+    });
 
-      const actualEvent = await Event.count({
-        where: { start: { [Op.lte]: new Date() }, end: { [Op.gte]: new Date() } },
-        include: { model: Group, where: { id: groupsId }, attributes: ['id'] },
-      });
+    const nextEvent = !actualEvent
+      ? await Event.findOne({ where: { start: { [Op.gte]: new Date() } }, include: [...eventIncludes], order: [['start', 'DESC']] })
+      : null;
 
-      const state = await State.findOne({ where: { slug: actualEvent > 0 ? 'actif' : 'pret' } });
+    const state = isStudent ? await State.findOne({ where: { slug: actualEvent ? 'actif' : 'pret' } }) : null;
 
+    if (actualEvent || nextEvent) {
+      const eventId = actualEvent?.id || nextEvent?.id;
+
+      // Add the state to the user
       if (state) {
         await user.setState(state);
+
         isDev && console.log(`${user.username} is now ready !`);
-        isDev && actualEvent > 0 && console.log(`${user.username} started the test ! (event ${payload.eventId})`);
+        isDev && eventId && console.log(`${user.username} joined the test ! (event ${eventId})`);
       }
-    } else {
-      await socket.join([`professor-event-${payload.eventId}`, `event-${payload.eventId}`]);
-      isDev && console.log(`${user.username} joined room professor-event-${payload.eventId} and event-${payload.eventId} as a professor.`);
+
+      // Make the user join the rooms
+      // rooms e.g : student-event-1 || professor-event-1     event-1
+      const rooms = [`${isStudent ? 'student' : 'professor'}-event-${eventId}`, `event-${eventId}`];
+
+      await socket.join(rooms);
+      isDev && console.log(`${user.username} joined room ${rooms.join(' and ')} as a ${isStudent ? 'student' : 'professor'}.`);
     }
   });
 };
