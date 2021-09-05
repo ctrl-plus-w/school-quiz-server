31c20d36018059ac381ff91acad5ba70af70db4f
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-09-05 19:05:43 +0200

    Made the user get warned when leaving the page if he got at least one question left

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,11 +1,14 @@
 import util from 'util';
 
 import { Socket } from 'socket.io';
+import { Op } from 'sequelize';
 
+import { UserAnswer } from '../models/userAnswer';
 import { EventWarn } from '../models/eventWarn';
-import { User } from '../models/user';
 import { Event } from '../models/event';
+import { User } from '../models/user';
 
+import { getValidQuestionConditions } from '../helpers/question.helper';
 import { userFormatter } from '../helpers/mapper.helper';
 import { getEvent } from '../helpers/socket.helper';
 
@@ -29,6 +32,9 @@ const onConnection = (_socket: Socket): void => {
     const user = socket.user;
     const role = socket.jwt.role;
 
+    // Only apply computations on students
+    if (role === 'professeur') return;
+
     await redisSetAsync(socket.user.id.toString(), JSON.stringify(STATES.OFFLINE));
 
     const [actualEvent, nextEvent] = await getEvent(user, role);
@@ -44,7 +50,24 @@ const onConnection = (_socket: Socket): void => {
         attributes: ['amount'],
       });
 
-      socket.to(`professor-event-${event.id}`).emit('user:update', { ...userFormatter(user), state: STATES.OFFLINE, eventWarns: [warn] });
+      const quiz = await event.getQuiz({ attributes: ['id'] });
+      const questions = await quiz.getQuestions({
+        include: { model: UserAnswer, where: { [Op.or]: [{ id: null }, { userId: { [Op.not]: user.id } }] }, required: false, attributes: [] },
+        where: { ...getValidQuestionConditions(user.id) },
+        attributes: [],
+      });
+
+      // Update the warns if the user leave the page and if there is an actual event
+      const newWarnAmount = warn ? warn.amount + 1 : 1;
+      const shouldUpdateWarn = actualEvent && newWarnAmount < 3 && questions.length > 0;
+
+      if (shouldUpdateWarn) {
+        await event.addWarnedUser(user, { through: { amount: newWarnAmount } });
+      }
+
+      socket
+        .to(`professor-event-${event.id}`)
+        .emit('user:update', { ...userFormatter(user), state: STATES.OFFLINE, eventWarns: [shouldUpdateWarn ? { amount: newWarnAmount } : warn] });
     }
   });
 };
